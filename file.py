962d548fe0e5bfa742f392d5450cda500a52cf00
unknown <harshitgupta23042002@gmail.com>
2023-07-06 19:44:53 +0530

    Initial Setup

---
+++ file.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+vid = cv.VideoCapture(0)
+
+while(True):
+    _, frame = vid.read()
+
+    cv.imshow("Camera", frame)
+
+    if(cv.waitKey(1) == ord('q')):
+       break
+
+vid.release()
+cv.destroyAllWindows()
